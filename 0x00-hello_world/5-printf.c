57050a22ea6c4a9a2e5588cca9d7e7c192ee4030
kaynzei <kanayo4christ@gmail.com>
2023-02-16 15:37:19 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - this is the main function
- *
- * Retrun: 0 when successfull
- */
+* main - this is the main function
+*
+* Retrun: 0 when successfull
+*/
 
  int main(void)
  {
- printf("with proper grammar, but the outcome is a piece of art,");
+ printf("with proper grammar, but the outcome is a piece of art,\n");
  return (0);
  }
