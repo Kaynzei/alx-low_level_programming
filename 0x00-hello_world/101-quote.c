ce3d7b8705dbabd1db8730e2aefb93114457e399
kaynzei <kanayo4christ@gmail.com>
2023-02-18 13:50:14 -0800

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - A program that prints a line to the standard error
- * Return: 1 (success)
- */
+* main - A program that prints a line to the standard error
+*
+* Return: 1 (success)
+*/
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora,
+korpar, 2015-10-19\n", stdout);
 	return (1);
 }
