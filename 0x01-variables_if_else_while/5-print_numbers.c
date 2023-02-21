00a3d7aef11b0f27cc76e7d9c6b5e8397f18754d
kaynzei <kanayo4christ@gmail.com>
2023-02-20 22:03:58 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A program that prints from zerot to ten
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
