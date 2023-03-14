ad02404700304e0e9f659006c8b6aa0cd4f9975c
kaynzei <kanayo4christ@gmail.com>
2023-03-14 16:23:51 -0700

    argv0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - this is  a function that prints it name
+ * @argc: argc parameter
+ * @argv: an array of a command listed
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+
+}
