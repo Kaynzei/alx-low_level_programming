c0e9114123a7fd4e112389cce74efef2ae23e257
kaynzei <kanayo4christ@gmail.com>
2023-04-10 16:44:03 -0700

    C-file I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int rs, p,len = 0;
+	int rs, p, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 	rs = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	p= write(rs, text_content, len);
+	p = write(rs, text_content, len);
 
 	if (rs == -1 || p == -1)
 		return (-1);
